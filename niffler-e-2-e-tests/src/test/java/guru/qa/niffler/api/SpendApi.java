b437c7b2339c0d9a55ce2d41a318cf380ae0d3e4
OctoTiger <dnazarov71317@gmail.com>
2024-09-12 22:18:08 +0400

    добавил ручки

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/SpendApi.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/SpendApi.java
@@ -1,12 +1,29 @@
 package guru.qa.niffler.api;
 
+import guru.qa.niffler.api.enums.Currency;
+import guru.qa.niffler.api.enums.Period;
 import guru.qa.niffler.model.SpendJson;
 import retrofit2.Call;
-import retrofit2.http.Body;
-import retrofit2.http.POST;
+import retrofit2.http.*;
+
+import java.util.List;
 
 public interface SpendApi {
 
-  @POST("internal/spends/add")
-  Call<SpendJson> addSpend(@Body SpendJson spend);
+    @POST("internal/spends/add")
+    Call<SpendJson> addSpend(@Body SpendJson spend);
+
+    @PATCH("internal/spends/edit")
+    Call<SpendJson> editSpend(@Body SpendJson spend);
+
+    @GET("internal/spends/{id}")
+    Call<SpendJson> getSpend(@Path("id") String id);
+
+    @GET("internal/spends/all")
+    Call<SpendJson> getAllSpend(@Query("filterPeriod") Period period, @Query("filterCurrency") Currency currency);
+
+    @DELETE("internal/spends/remove")
+    Call<Void> deleteSpend(@Query("ids") List<String> ids);
+
+
 }
